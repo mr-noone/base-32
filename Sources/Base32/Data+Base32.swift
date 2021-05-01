2fb208392246119cb15e93bf9aba3902fa394b8a
Aleksey Zgurskiy <mr.noone@icloud.com>
2021-05-01 14:59:43 +0300

    Fix encoding to string

--- Sources/Base32/Data+Base32.swift
+++ Sources/Base32/Data+Base32.swift
@@ -7,20 +7,17 @@ extension Data {
 
 public extension Data {
   init?(base32Encoded string: String) {
-    guard let data = string.data(using: .ascii)?.bytes else { return nil }
-    let bufferSize = (string.count + 7) / 8 * Int(BITS_PER_BASE32_CHAR)
+    guard let data = string.data(using: .utf8)?.bytes else { return nil }
+    let bufferSize = (data.count * Int(BITS_PER_BASE32_CHAR) + 4) / 8
     var buffer = Array<UInt8>(repeating: 0x00, count: bufferSize)
     if base32_decode(data, &buffer, Int32(bufferSize)) < 1 { return nil }
     self.init(bytes: buffer, count: bufferSize)
   }
   
   func base32EncodedString() -> String? {
-    let bufferSize = bytes.count * 8 / Int(BITS_PER_BASE32_CHAR)
+    let bufferSize = (count * 8 + 4) / Int(BITS_PER_BASE32_CHAR)
     var buffer = Array<UInt8>(repeating: 0x00, count: bufferSize)
-    if base32_encode(bytes, Int32(bytes.count), &buffer, Int32(bufferSize)) < 1 {
-      return nil
-    }
-    
-    return String(bytes: buffer, encoding: .ascii)
+    if base32_encode(bytes, Int32(count), &buffer, Int32(bufferSize)) < 1 { return nil }
+    return String(bytes: buffer, encoding: .utf8)
   }
 }
--- Tests/Base32Tests/Base32Tests.swift
+++ Tests/Base32Tests/Base32Tests.swift
@@ -8,17 +8,13 @@ final class Base32Tests: XCTestCase {
   ]
   
   func testBase32Decoding() {
-    let testBytes: [UInt8] = [72, 101, 108, 108, 111, 33, 222, 173, 190, 239]
-    let testData = Data(bytes: testBytes, count: testBytes.count)
-    let data = Data(base32Encoded: "JBSWY3DPEHPK3PXP")
-    XCTAssertEqual(data, testData)
+    let one = "QWERTY".data(using: .utf8)
+    let two = Data(base32Encoded: "KFLUKUSULE")
+    XCTAssertEqual(one, two)
   }
   
   func testBase32Encoding() {
-    let testBytes: [UInt8] = [72, 101, 108, 108, 111, 33, 222, 173, 190, 239]
-    let base32Str = "JBSWY3DPEHPK3PXP"
-    
-    let data = Data(bytes: testBytes, count: testBytes.count)
-    XCTAssertEqual(data.base32EncodedString(), base32Str)
+    let data = "QWERTY".data(using: .utf8)
+    XCTAssertEqual(data?.base32EncodedString(), "KFLUKUSULE")
   }
 }
